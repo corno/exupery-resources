8928fceb42c43ee4dd6e193828c5abf67c0057dd
Corno Schraverus <corno@schraverus.com>
2025-09-30 19:35:02 +0200

    .

---
+++ src/procedures/test.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import * as _ea from 'exupery-core-alg'
+import * as _eb from 'exupery-core-bin'
+import * as _easync from 'exupery-core-async'
+
+export const $$: _eb.Run_Unguaranteed_Procedure_Main = ($i) => $i.execute(($i) => $i )
