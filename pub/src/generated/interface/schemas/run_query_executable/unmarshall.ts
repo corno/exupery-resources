f9e8dafbbeb28a3257abc09d2894357b87d8fad3
Corno Schraverus <corno@schraverus.com>
2025-10-27 19:26:19 +0100

    .

---
+++ pub/src/generated/interface/schemas/run_query_executable/unmarshall.ts
@@ -0,0 +1,148 @@
+import * as _pt from 'exupery-core-types'
+
+import * as _i_in from "../../core/astn_source"
+import * as _i_out from "./data_types/target"
+import * as _i_vd from "./value_deserializers"
+
+// **** TYPES
+
+export type _T_Errors = (
+    $$_: _i_in._T_Value,
+    $$_p: {
+        readonly 'value deserializers': _i_vd._T_Value_Deserializers
+    },
+) => _i_out._T_Errors
+
+export type _T_Parameters = (
+    $$_: _i_in._T_Value,
+    $$_p: {
+        readonly 'value deserializers': _i_vd._T_Value_Deserializers
+    },
+) => _i_out._T_Parameters
+
+export type _T_Result = (
+    $$_: _i_in._T_Value,
+    $$_p: {
+        readonly 'value deserializers': _i_vd._T_Value_Deserializers
+    },
+) => _i_out._T_Result
+
+// **** FRIENDLY NAMES FOR THE GLOBAL TYPES
+
+export type Errors = _T_Errors
+
+export type Parameters = _T_Parameters
+
+export type Result = _T_Result
+
+// **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
+
+export namespace _T_Errors {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Errors
+}
+
+export namespace _T_Parameters {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Parameters
+}
+
+export namespace _T_Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Result
+}
+
+// *** ALIASES FOR NESTED TYPES
+
+export namespace Errors {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Errors
+}
+
+export namespace Parameters {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Parameters
+}
+
+export namespace Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Value
+    
+    export namespace PARAMS {
+        
+        export namespace value_deserializers {
+        }
+        export type value_deserializers = _i_vd._T_Value_Deserializers
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Result
+}
